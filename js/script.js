17688cd316da55945b97d5f1ac72f71495f07edf
Prince Roy <princeku.ray143@gmail.com>
2022-08-25 01:07:56 +0530

    fecth promise and display data

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
 'use strict';
 
 console.log('started.....');
+const adviceId = document.querySelector('.advice--id');
+const adviceText = document.querySelector('.advice--text');
 
-const res = await fetch(`https://api.adviceslip.com/advice`);
-const data = await res.json();
-console.log(data);
+const getAdviceAndDisplay = async() => {
+    try {
+        const res = await fetch('https://api.adviceslip.com/advice');
+        const data = await res.json();
+
+        const { id, advice } = data.slip;
+        adviceId.textContent = id;
+        adviceText.textContent = advice;
+    } catch (err) {
+        adviceId.textContent = err;
+        adviceText.textContent = err;
+    }
+};
+
+getAdviceAndDisplay();
