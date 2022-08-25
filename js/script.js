67d0a0564c72757c8bf1d03d01414aa2266c05cd
Prince Roy <princeku.ray143@gmail.com>
2022-08-25 15:40:25 +0530

    update func to popup advice

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
 'use strict';
 
 console.log('started.....');
-const adviceId = document.querySelector('.advice--id');
+const cardContainer = document.querySelector('.advice__card');
+const adviceId = document.querySelector('.id--num');
 const adviceText = document.querySelector('.advice--text');
+const diceBtn = document.querySelector('.btn--dice');
 
 const getAdviceAndDisplay = async() => {
     try {
         const res = await fetch('https://api.adviceslip.com/advice');
+
         const data = await res.json();
 
         const { id, advice } = data.slip;
-        adviceId.textContent = id;
+        adviceId.textContent = `#${id}`;
         adviceText.textContent = advice;
     } catch (err) {
-        adviceId.textContent = err;
-        adviceText.textContent = err;
+        console.log(err.message);
+        adviceId.textContent = err.type;
+        adviceText.textContent = err.message;
+        throw err;
     }
 };
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        cardContainer.classList.add('show');
+        cardContainer.nextElementSibling.style.display = 'none';
+    }, 2000);
+});
+
+diceBtn.addEventListener('click', getAdviceAndDisplay);
+window.addEventListener('keydown', e => {
+    console.log(e.key);
+    e.key === 'Enter' && getAdviceAndDisplay();
+});
 
 getAdviceAndDisplay();
